57c991180ffa77f8d2aec240b3f35e424a538006
mnt3710 <tomatomnt@gmail.com>
2021-07-08 19:10:47 +0900

    Initial revision

---
+++ dice.java
@@ -0,0 +1,17 @@
+public class dice{
+  public static void main(String[] args){
+
+System.out.println("Rolling the dice...");
+      int firstDice = 0;
+      int secondDice = 0;
+      int totalValue = 0;
+for(int i = 1;i <= 2; i++){
+    int randomValue = new java.util.Random().nextInt(7);
+    int diceValue = randomValue;
+  System.out.println("Die " + i + ":" + diceValue);
+  totalValue += diceValue;
+}
+
+System.out.println("Total value: " + totalValue);
+  }
+}
